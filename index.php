fa6161094257f09ae98518c3312f47008eccdbbd
pashtet2233 <97891994+pashtet2233@users.noreply.github.com>
2022-01-17 16:05:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="send.php" method="post">
+	<input type="text" name="login" placeholder="введите логин" />
+
+	<input type="password" name="pass" placeholder="введите пароль" />
+	<input type="email" name="email" placeholder="введите email">
+	<input type="tel" name="tel" placeholder="введите телефон" />
+	<input type="submit" value="отправить данные" />
+
+</form>
