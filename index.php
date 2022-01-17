d7fe743d87c3d11e6fa6154b65e94d1bd91b271c
pashtet2233 <97891994+pashtet2233@users.noreply.github.com>
2022-01-17 16:12:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<form action="send.php" method="post">
-	<input type="text" name="login" placeholder="введите логин" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form action="send.php" method="post">
+	 <input type="text" name="login" placeholder="введите логин" />
 
-	<input type="password" name="pass" placeholder="введите пароль" />
-	<input type="email" name="email" placeholder="введите email">
-	<input type="tel" name="tel" placeholder="введите телефон" />
-	<input type="submit" value="отправить данные" />
-
-</form>
+	 <input type="password" name="pass" placeholder="введите пароль" />
+	 <input type="email" name="email" placeholder="введите email">
+	 <input type="tel" name="tel" placeholder="введите телефон" />
+	 <input type="submit" value="отправить данные" />
+    </form>
+</body>
+</html>
